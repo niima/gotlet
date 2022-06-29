95e5f896aab081507aef49dfe8658bc1769c4e4c
Khashayar Danesh <k@idlethread.net>
2022-06-29 21:24:40 +0200

    Updated documents, changed flag names, changed exit codes on some validations

--- main.go
+++ main.go
@@ -9,33 +9,41 @@ import (
 )
 
 func main() {
-	if len(flag.Args()) == 0 {
-		fmt.Println("gotlet renders your variables into a template file.")
+	flag.Usage = func() {
+		Output := flag.CommandLine.Output()
+		fmt.Fprintf(Output, "Usage of Simple commandline templating utility %s: \n", os.Args[0])
+		flag.PrintDefaults()
 	}
 
-	templateFile := flag.String("t", "", "Specify the template file path")
-	envPrefix := flag.String("p", "", "A prefix for filtering which env variables to include")
-	dataFile := flag.String("d", "", "Specify the data file containing the variables in Yaml")
-	outputPath := flag.String("o", "result.yaml", "Output file path")
-	printOutput := flag.Bool("v", false, "Print out the result in stdout")
+	templateFile := flag.String("template", "", "[Required] Path to the template file.")
+	envPrefix := flag.String("envprefix", "", "Prefix of the OS environment variables you want to use during templating, if none are provided, all of the environment variables will be imported")
+	variablesFile := flag.String("varsfile", "", "Path to the file containing the variables in YAML format, if none are provided, template rendering will fall back to environment variables")
+	outputFile := flag.String("output", "", "Path to the the file in which the rendered template will be stored in, defaults to stdout if no file name is provided.")
 
 	flag.Parse()
 
+	// Exit with error code 2 (invalid usage of shell command)
+	if len(os.Args[1:]) <= 0 {
+		flag.Usage()
+		os.Exit(2)
+	}
+
+	// Exit with error code 2 (invalid usage of shell command)
 	if *templateFile == "" {
 		fmt.Println(colors.Red + "- The template file path is required, use -t flag")
-		os.Exit(1)
+		os.Exit(2)
 	}
 
 	if *envPrefix == "" {
 		fmt.Println(colors.Yellow, "- No env variable prefix specified, loading all env variables.\n"+
-			"\tThis is not recommended, use -p flag to specify", colors.Reset)
+			"This is not recommended, use -p flag to specify", colors.Reset)
 	}
 
 	data := &Model{
 		Variables: make(map[string]any),
 	}
 
-	err := getVariables(data, *dataFile)
+	err := getVariables(data, *variablesFile)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
@@ -49,15 +57,13 @@ func main() {
 		os.Exit(1)
 	}
 
-	if *outputPath != "" {
-		err = ioutil.WriteFile(*outputPath, result, 0644)
+	if *outputFile != "" {
+		err = ioutil.WriteFile(*outputFile, result, 0644)
 		if err != nil {
 			fmt.Println(err)
 			os.Exit(1)
 		}
-	}
-
-	if *printOutput {
+	} else {
 		fmt.Println(string(result))
 	}
 }
